fe4ea996e0af9344a617fa9e3086f2c03ae87ae5
frontEndDevel <front_end_dev@yahoo.com>
2021-06-22 00:05:20 +0300

    Updates

---
+++ static/js/main.js
@@ -0,0 +1,2 @@
+"use strict";$(function(){});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsiJCJdLCJtYXBwaW5ncyI6ImFBQUVBLEVBQUUiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIjsgJChmdW5jdGlvbiAoKSB7XHJcblxyXG5cclxufSk7Il19
